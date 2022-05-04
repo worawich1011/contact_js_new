d7facfeea72ddbb54cc38e0f2fd79441db4f04d6
worawich1011 <worawich1011@gmail.com>
2022-05-04 16:48:07 +0700

    finish

--- www/app.js
+++ www/app.js
@@ -1,56 +1,56 @@
-const charactersList = document.getElementById('charactersList');
+const TeacherList = document.getElementById('TeacherList');
 const searchBar = document.getElementById('searchBar');
-let hpCharacters = [];
+let hpTeachers = [];
 
 searchBar.addEventListener('keyup', (e) => {
     const searchString = e.target.value.toLowerCase();
 
-    const filteredCharacters = hpCharacters.filter((character) => {
+    const filteredTeachers = hpTeachers.filter((teacher) => {
         return (
-            character.name.toLowerCase().includes(searchString) ||
-            character.email.toLowerCase().includes(searchString)
+            teacher.name.toLowerCase().includes(searchString) ||
+            teacher.email.toLowerCase().includes(searchString)
         );
     });
-    displayCharacters(filteredCharacters);
+    displayteachers(filteredTeachers);
 });
-
-const loadCharacters = async () => {
+f
+const loadteachers = async () => {
     try {
         const res = await fetch('http://localhost:5000/teacher');
-        hpCharacters = await res.json();
-        displayCharacters(hpCharacters);
+        hpTeachers = await res.json();
+        displayteachers(hpTeachers);
 
-        displayCharacters();
+        displayteachers();
     } catch (err) {
         console.error(err);
     }
 };
 
-const displayCharacters = (characters) => {
-    const htmlString = characters
-        .map((character) => {
+const displayteachers = (teachers) => {
+    const htmlString = teachers
+        .map((teacher) => {
             return `
-            <li onclick="onPopup(this)" class="character">
-                <h2 class="name">${character.name}</h2> 
-                <p class="email">email: ${character.email}</p>
-                <img class="img" src="${character.image}"></img>
+            <li onclick="onPopup(this)" class="teacher">
+                <h2 class="name">${teacher.name}</h2> 
+                <p class="email">email: ${teacher.email}</p>
+                <img class="img" src="${teacher.image}"></img>
             </li>
         `;
         })
         .join('');
-    charactersList.innerHTML = htmlString;
+    TeacherList.innerHTML = htmlString;
 };
 
-loadCharacters();
+loadteachers();
 
 var popupContainer = document.getElementsByClassName('pop-cont')[0],
     popupBox = document.getElementsByClassName('pop-up')[0];
 
 
-function onPopup(character) {
-    let name = character.querySelector('.name').innerHTML;
-    let email = character.querySelector('.email').innerHTML;
-    let img = character.querySelector('.img').src;
+function onPopup(teacher) {
+    let name = teacher.querySelector('.name').innerHTML;
+    let email = teacher.querySelector('.email').innerHTML;
+    let img = teacher.querySelector('.img').src;
 
     document.querySelector('.pop-name').innerHTML = name;
     document.querySelector('.pop-email').innerHTML = email;
